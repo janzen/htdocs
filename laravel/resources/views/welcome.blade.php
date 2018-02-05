ae626a419a55af163f1d089e4d3d2df864ddc145
janzen <349685719@qq.com>
2018-02-05 16:37:49 +0800

    初始

---
+++ laravel/resources/views/welcome.blade.php
@@ -0,0 +1,103 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>禾描-互联网实木家具定制品牌</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @if (Auth::check())
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ url('/login') }}">Login</a>
+                        <a href="{{ url('/register') }}">Register</a>
+                    @endif
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    禾描
+                </div>
+
+                <div>
+                    <p>————————————</p>
+                    <p>把空气做成不同的盒子</p>
+                    <p>让灵感自由徜徉</p>
+                    <p>是梦想变为真实</p>
+                    <p>由建筑师／设计师／诗人／互联网工程师</p>
+                    <p>共同创立的实木家具定制品牌</p>
+                    <p>更理解空间</p>
+                    <p>更热爱家</p>
+                    <p>更关怀你</p>
+                    <p>————————————</p>
+                </div>
+                <div>
+                    <a href="https://shop142723414.taobao.com">淘宝店铺</a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
