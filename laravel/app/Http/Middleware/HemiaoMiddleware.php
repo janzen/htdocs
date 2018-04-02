24bc12e06d29b90b5c57bc907411f361c9877903
janzen <349685719@qq.com>
2018-04-02 12:57:48 +0800

    后台

--- laravel/app/Http/Middleware/HemiaoMiddleware.php
+++ laravel/app/Http/Middleware/HemiaoMiddleware.php
@@ -16,9 +16,9 @@ class HemiaoMiddleware
     public function handle($request, Closure $next)
     {
         //记录请求路径
-        $str = '['.date('Y-m-d H:i:s').']'.$request->ip().'------'.$request->path();
+        // $str = '['.date('Y-m-d H:i:s').']'.$request->ip().'------'.$request->path();
         //存入日志文件
-        file_put_contents('request.log', $str."\r\n",FILE_APPEND);
+        // file_put_contents('request.log', $str."\r\n",FILE_APPEND);
         return $next($request);
     }
 }
