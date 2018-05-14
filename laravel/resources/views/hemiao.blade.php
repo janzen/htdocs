9fe29ab9ca1208ddc866517e729a38a7e2a75ac2
janzen <349685719@qq.com>
2018-05-14 12:41:17 +0800

    图片懒加载

--- laravel/resources/views/hemiao.blade.php
+++ laravel/resources/views/hemiao.blade.php
@@ -7,12 +7,18 @@
 <meta charset="utf-8">
 <title>禾描-实木定制</title>
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
-<meta name=”keywords” content=“禾描|实木定制|实木家具定制|实木衣柜定制|实木直拼|实木平拼|实木榻榻米定制|环保|白橡|黑胡桃|木蜡油”>
-<meta name=”description” content=“禾描|实木定制|实木家具定制|实木衣柜定制|实木直拼|实木平拼|实木榻榻米定制|环保|白橡|黑胡桃|木蜡油”>
+<meta name=”keywords” content=“禾描,实木定制,实木家具定制,实木衣柜定制,实木直拼,实木平拼,实木榻榻米定制,环保,白橡,黑胡桃,木蜡油,整木定制”>
+<meta name=”description” content=“描®诞生于2015年7月 北京 懂空间的建筑师 爱生活的家具设计师 以及关注用户体验的互联网人 因为有着共同的居住理想“诗意地栖居于大地之上” 他们一起创立了“禾描”实木家具设计品牌。实木定制,实木家具定制,实木衣柜定制,实木直拼,实木平拼,实木榻榻米定制,环保,白橡,黑胡桃,木蜡油,整木定制”>
 <link rel="stylesheet" href="css/bootstrap.min.css">
-    
+<script src="js/jquery.js"></script>
+<script src="js/jquery.lazyload.js"></script>
 <!-- Main CSS -->
 <link rel="stylesheet" href="css/main.css">
+<script type="text/javascript" charset="utf-8">
+  $(function() {
+      $("img").lazyload({ threshold :200});
+  });
+</script>
 </head>
 <body id="home" data-spy="scroll" data-offset="50" data-target=".navbar-default">
 
@@ -36,16 +42,16 @@
         <span class="icon-bar"></span>
       </button>
       <a href="#home">
-        <img src="img/logo.png" style="width: 160px;margin-left: -15px;" alt="LOGO" />
+        <img data-original="img/logo.png" style="width: 160px;margin-left: -15px;margin-top: 10px;" alt="LOGO" />
       </a>
     </div>
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <div class="collapse navbar-collapse is-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav navbar-right" style="margin-top: 15px;">
         <li><a href="#home" class="smoothScroll">首页</a></li>
             <li><a href="#portfolio" class="smoothScroll">产品</a></li>
             <li><a href="#blog" class="smoothScroll">服务</a></li>
             <li><a href="#about" class="smoothScroll">关于我们</a></li>
-            <li><a target="_blank" href="https://shop142723414.taobao.com" class="smoothScroll">淘宝</a></li>
+            <li><a target="_blank" href="http://shop111117.top/eDG4KS" class="smoothScroll">淘宝</a></li>
       </ul>
     </div>
   </div>
@@ -55,7 +61,7 @@
 <header class="revolution-home" >
   <div class="container">
     <div class="row">
-      <img src="img/sliders/notgenericherobg.png"/>
+      <img data-original="img/sliders/notgenericherobg.png"/>
       <div class="col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-6 col-xs-offset-3 big_title_home">
       <p style="text-align: center;">探索空间&nbsp;&nbsp;感受生活</p>
     </div>
@@ -76,14 +82,14 @@
   </div>
   <div class="container">
     <div class="row">
-      <img src="/img/sliders/team-bg-1.png"/>
+      <img data-original="/img/sliders/team-bg-1.png"/>
     </div>
   </div>
 <div class="container">
     <div class="row">
       <div class="col-md-3 col-sm-6 col-xs-6">
         <div class="feature">
-          <img class="img-responsive center-block img_circle" src="/img/material/background_160_1.png">
+          <img class="img-responsive center-block img_circle" data-original="/img/material/background_160_1.png">
           <div class="img_title_p_back">
             <p class="material_title">原创设计</p>
           </div>
@@ -92,7 +98,7 @@
       </div>
       <div class="col-md-3 col-sm-6 col-xs-6">
         <div class="feature">
-          <img class="img-responsive center-block img_circle" src="/img/material/background_160_2.png">
+          <img class="img-responsive center-block img_circle" data-original="/img/material/background_160_2.png">
           <div class="img_title_p_back">
           <p class="material_title">整体搭配</p>
         </div>
@@ -100,7 +106,7 @@
       </div>
       <div class="col-md-3 col-sm-6 col-xs-6">
         <div class="feature">
-          <img class="img-responsive center-block img_circle" src="/img/material/background_160_3.png">
+          <img class="img-responsive center-block img_circle" data-original="/img/material/background_160_3.png">
           <div class="img_title_p_back">
           <p class="material_title">圆角设计</p>
         </div>
@@ -108,7 +114,7 @@
       </div>
       <div class="col-md-3 col-sm-6 col-xs-6">
         <div class="feature">
-          <img class="img-responsive center-block img_circle" src="/img/material/background_160_4.png">
+          <img class="img-responsive center-block img_circle" data-original="/img/material/background_160_4.png">
           <div class="img_title_p_back">
           <p class="material_title">关注细节</p>
         </div>
@@ -132,21 +138,21 @@
           <div class="feature">
           <p class="mucai_title">精选北美进口硬木</p>
               <div class="col-md-3 col-md-offset-2 margin-15px">
-                  <img  class="img-responsive center-block img_circle_mu" src="/img/material/baixiangmu.png">
+                  <img  class="img-responsive center-block img_circle_mu" data-original="/img/material/baixiangmu.png">
                   <div class="img_title_p_back">
                   <p class="material_title">白橡</p>
                 </div>
             </div>
             
               <div class="col-md-2 margin-15px">
-                  <img  class="img-responsive center-block img_circle_mu" src="/img/material/heihutaomu.png">
+                  <img  class="img-responsive center-block img_circle_mu" data-original="/img/material/heihutaomu.png">
                  <div class="img_title_p_back">
                   <p class="material_title">黑胡桃</p>
                 </div>
             </div>
             
               <div class="col-md-3 margin-15px">
-                  <img  class="img-responsive center-block img_circle_mu" src="/img/material/bailamu.png">
+                  <img  class="img-responsive center-block img_circle_mu" data-original="/img/material/bailamu.png">
                   <div class="img_title_p_back">
                   <p class="material_title">白蜡</p>
                 </div>
@@ -159,13 +165,13 @@
               <div class="col-md-6 col-xs-12">
                 <p class="fas_title" style="padding-top: 30px;">只用FAS级木材</p>
                 <div class="col-md-6 col-xs-6 fas_log">
-                  <img class="img-responsive center-block img_circle_mu_ji" src="/img/material/fas.png">
+                  <img class="img-responsive center-block img_circle_mu_ji" data-original="/img/material/fas.png">
                   <div class="img_title_p_back_mu">
                   <p>FAS级</p>
                 </div>
                 </div>
                 <div class="col-md-6 col-xs-6 putong_logo">
-                  <img class="img-responsive center-block img_circle_mu_ji" src="/img/material/putongyiji.png">
+                  <img class="img-responsive center-block img_circle_mu_ji" data-original="/img/material/putongyiji.png">
                   <div class="img_title_p_back_mu">
                   <p>普通一级</p>
                 </div>
@@ -175,13 +181,13 @@
               <div class="col-md-6 col-xs-12">
                 <p class="zhipin_title padding-15px" style="padding-top: 30px;">只使用直拼工艺</p>
                 <div class="col-md-6 col-xs-6 fas_log">
-                  <img class="img-responsive center-block img_circle_mu_ji" src="/img/material/zhipinban.png">
+                  <img class="img-responsive center-block img_circle_mu_ji" data-original="/img/material/zhipinban.png">
                   <div class="img_title_p_back_mu">
                   <p>直拼板</p>
                 </div>
                 </div>
                 <div class="col-md-6 col-xs-6 putong_logo">
-                    <img class="img-responsive center-block img_circle_mu_ji" src="/img/material/zhijieban.png">
+                    <img class="img-responsive center-block img_circle_mu_ji" data-original="/img/material/zhijieban.png">
                   <div class="img_title_p_back_mu">
                     <p>指接板</p>
                   </div>
@@ -195,7 +201,7 @@
           <div class="feature">
             <p class="osmo_title margin-15px">德国植物性木蜡油</p>
               <div class="col-md-4 col-md-offset-2 osmo_logo margin-15px">
-                <img width="160px" src="/img/material/osmo.png">
+                <img width="160px" data-original="/img/material/osmo.png">
               </div>
             <div class="col-md-4 osmo_content">
               <p>禾描®只用德国欧诗木木蜡油<br/>木蜡油是以天然的植物油和蜡制成<br/>安全无毒，通过食品安全等级<br/>可以放心使用在儿童和宠物的环境</p>
@@ -214,7 +220,7 @@
         </div>
       </div>
     <div class="row">
-      <img src="/img/sliders/team-bg-3.png"/>
+      <img data-original="/img/sliders/team-bg-3.png"/>
     </div>
 
     <div class="row">
@@ -222,19 +228,19 @@
         <div class="feature">
           <p class="wujinpeijian_title">全球优选五金配件</p>
           <div class="col-md-3 col-xs-6 margin-15px">
-              <img class="img-responsive center-block img_circle_wujin" src="/img/material/hettich.png">
+              <img class="img-responsive center-block img_circle_wujin" data-original="/img/material/hettich.png">
               <p class="material_title margin-15px">海蒂诗</p>
           </div>
           <div class="col-md-3 col-xs-6 margin-15px">
-              <img class="img-responsive center-block img_circle_wujin" src="/img/material/hafele.png">
+              <img class="img-responsive center-block img_circle_wujin" data-original="/img/material/hafele.png">
               <p class="material_title margin-15px">海福乐</p>
           </div>
           <div class="col-md-3 col-xs-6 margin-15px">
-              <img class="img-responsive center-block img_circle_wujin" src="/img/material/indaux.png">
+              <img class="img-responsive center-block img_circle_wujin" data-original="/img/material/indaux.png">
               <p class="material_title margin-15px">Indaux</p>
           </div>
           <div class="col-md-3 col-xs-6 margin-15px">
-              <img class="img-responsive center-block img_circle_wujin" src="/img/material/fischer.png">
+              <img class="img-responsive center-block img_circle_wujin" data-original="/img/material/fischer.png">
               <p class="material_title margin-15px">慧鱼</p>
           </div>
         </div>
@@ -255,28 +261,28 @@
     </div>
     <div class="row">
       <div class="col-md-6">
-          <img class="img-responsive center-block padding-15px" src="/img/material/anli_1.png">
+          <img class="img-responsive center-block padding-15px" data-original="/img/material/anli_1.png">
             <div class="col-md-12 col-xs-12 box_background">
               <div class="col-md-9 col-xs-9 box_content_div">
               <p class="box_p_1">找不到合适的家具，像他们一样定制吧</p>
               <p class="box_p_2">一房，两人，三只喵，他们选择定制家具</p>
               </div>
               <div  class="col-md-3 col-xs-3 box_ma_div">
-                <img class="img-responsive center-block box_img" src="/img/material/anli_ma_1.png">
+                <img class="img-responsive center-block box_img" data-original="/img/material/anli_ma_1.png">
                 <span class="box_span">了解更多</span>
               </div>
             </div>
       </div>
 
       <div class="col-md-6">
-        <img class="img-responsive center-block padding-15px" src="/img/material/anli_3.png">
+        <img class="img-responsive center-block padding-15px" data-original="/img/material/anli_3.png">
             <div class="col-md-12 col-xs-12 box_background">
               <div class="col-md-9 col-xs-9 box_content_div">
               <p class="box_p_1">咏春 VS 八卦，客厅改造健身房</p>
               <p class="box_p_2">温暖有爱的家</p>
               </div>
               <div  class="col-md-3 col-xs-3 box_ma_div">
-                <img class="img-responsive center-block box_img" src="/img/material/anli_ma_3.png">
+                <img class="img-responsive center-block box_img" data-original="/img/material/anli_ma_3.png">
                 <span class="box_span">了解更多</span>
               </div>
             </div>
@@ -284,28 +290,28 @@
     </div>
     <div class="row">
       <div class="col-md-6">
-          <img class="img-responsive center-block padding-15px" src="/img/material/anli_2.png">
+          <img class="img-responsive center-block padding-15px" data-original="/img/material/anli_2.png">
             <div class="col-md-12 col-xs-12 box_background">
               <div class="col-md-9 col-xs-9 box_content_div">
               <p class="box_p_1">一室变三室，她的空间魔法</p>
               <p class="box_p_2">空间小不是大问题</p>
               </div>
               <div  class="col-md-3 col-xs-3 box_ma_div">
-                <img class="img-responsive center-block box_img" src="/img/material/anli_ma_2.png">
+                <img class="img-responsive center-block box_img" data-original="/img/material/anli_ma_2.png">
                 <span class="box_span">了解更多</span>
               </div>
             </div>
       </div>
 
       <div class="col-md-6">
-        <img class="img-responsive center-block padding-15px" src="/img/material/anli_4.png">
+        <img class="img-responsive center-block padding-15px" data-original="/img/material/anli_4.png">
             <div class="col-md-12 col-xs-12 box_background">
               <div class="col-md-9 col-xs-9 box_content_div">
               <p class="box_p_1">客厅不以电视为中心是一种怎样的体验</p>
               <p class="box_p_2">家与工作室的完美结合</p>
               </div>
               <div  class="col-md-3 col-xs-3 box_ma_div">
-                <img class="img-responsive center-block box_img" src="/img/material/anli_ma_4.png">
+                <img class="img-responsive center-block box_img" data-original="/img/material/anli_ma_4.png">
                 <span class="box_span">了解更多</span>
               </div>
             </div>
@@ -326,28 +332,28 @@
     <div class="row">
       <div class="col-md-3 col-xs-6">
         <div class="feature">
-          <img class="img-responsive center-block" style="height: 50px;padding-left: 20px;" src="/img/material/22.png">
+          <img class="img-responsive center-block" style="height: 50px;padding-left: 20px;" data-original="/img/material/22.png">
           <p style="font-size: 40px;font-weight: 800;">1</p>
           <p class="material_title">沟通需求&nbsp上门量房</p>
         </div>
       </div>
       <div class="col-md-3 col-xs-6">
         <div class="feature">
-          <img class="img-responsive center-block"  style="height: 50px" src="/img/material/23.png">
+          <img class="img-responsive center-block"  style="height: 50px" data-original="/img/material/23.png">
           <p style="font-size: 40px;font-weight: 800;">2</p>
           <p class="material_title">免费设计&nbsp确定方案</p>
         </div>
       </div>
       <div class="col-md-3 col-xs-6">
         <div class="feature">
-          <img class="img-responsive center-block"  style="height: 50px;padding-left: 5px;" src="/img/material/24.png">
+          <img class="img-responsive center-block"  style="height: 50px;padding-left: 5px;" data-original="/img/material/24.png">
           <p style="font-size: 40px;font-weight: 800;">3</p>
           <p class="material_title">签订合同&nbsp制作生产</p>
         </div>
       </div>
       <div class="col-md-3 col-xs-6">
         <div class="feature">
-          <img class="img-responsive center-block"  style="height: 50px;padding-right: 10px;" src="/img/material/25.png">
+          <img class="img-responsive center-block"  style="height: 50px;padding-right: 10px;" data-original="/img/material/25.png">
           <p style="font-size: 40px;font-weight: 800;">4</p>
           <p class="material_title">送货安装&nbsp无忧售后</p>
         </div>
@@ -377,11 +383,11 @@
     <div class="row">
       <div class="col-md-12">
         <div class="col-md-2 col-md-offset-4">
-          <img class="img-responsive center-block" style="width: 100px" src="/img/material/wx_ma.png" alt="禾描公众号" />
+          <img class="img-responsive center-block" style="width: 100px" data-original="/img/material/wx_ma.png" alt="禾描公众号" />
           <p style="font-weight: lighter;color: #7b7b7b;">微信公众号</p>
         </div>
         <div class="col-md-2">
-          <img class="img-responsive center-block" style="width: 100px" src="/img/material/wx_ma.png" alt="禾描公众号" />
+          <img class="img-responsive center-block" style="width: 100px" data-original="/img/material/wx_ma.png" alt="禾描公众号" />
           <p style="font-weight: lighter;color: #7b7b7b;">淘宝店铺</p>
         </div>
       </div>
@@ -393,9 +399,7 @@
     <p>商务合作：homerus@homerus.com</p>
 
 </div>
-<script src="js/jquery.js"></script>
 <script src="js/vendor/fastclick.js"></script>
-
 <script src="js/vendor/jquery.appear.js"></script>                  <!-- jQuery Appear -->
 <script src="js/vendor/imagesloaded.pkgd.min.js"></script>          <!-- Imagesloaded -->
 <script src="js/vendor/isotope.pkgd.min.js"></script>               <!-- Isotope -->
