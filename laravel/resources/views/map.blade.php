6561e37de3eaeb0b3e25efe6ee0fcd1ef7ccf85b
janzen <349685719@qq.com>
2018-05-29 19:30:06 +0800

    wx

--- laravel/resources/views/map.blade.php
+++ laravel/resources/views/map.blade.php
@@ -14,7 +14,7 @@
     <script src="http://cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script><!--调用jQuery-->
         <script type="text/javascript">  
 wx.config({
-    debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
     appId: <?echo "'".$appId."'"?>, // 必填，公众号的唯一标识
     timestamp: <?echo "'".$timestamp."'"?>, // 必填，生成签名的时间戳
     nonceStr: <?echo "'".$nonceStr."'"?>, // 必填，生成签名的随机串
