236f17e9cd461b61b32bc01935771559bdd21e79
janzen <349685719@qq.com>
2018-05-29 21:31:30 +0800

    wx

--- laravel/resources/views/map.blade.php
+++ laravel/resources/views/map.blade.php
@@ -4,15 +4,53 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>禾描-实木定制</title>
-        <meta name=”keywords” content=“禾描|实木定制|实木家具定制|实木衣柜定制|实木直拼|实木平拼|实木榻榻米定制|环保|白橡|黑胡桃|木蜡油”>
-        <meta name=”description” content=“禾描|实木定制|实木家具定制|实木衣柜定制|实木直拼|实木平拼|实木榻榻米定制|环保|白橡|黑胡桃|木蜡油”>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <title>禾描</title>
         <script src="js/jquery.js"></script>
+        <style type="text/css">
+            button.btn {
+                width: 100%;
+                border: 0;
+                outline: 0;
+                -webkit-appearance: none;
+            }
 
+            .btn {
+                margin-bottom: 15px;
+            }
+            .btn_primary {
+                background-color: #04be02;
+            }
+            .btn {
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+                padding-left: 14px;
+                padding-right: 14px;
+                font-size: 18px;
+                text-align: center;
+                text-decoration: none;
+                overflow: visible;
+                height: 42px;
+                border-radius: 5px;
+                -moz-border-radius: 5px;
+                -webkit-border-radius: 5px;
+                box-sizing: border-box;
+                -moz-box-sizing: border-box;
+                -webkit-box-sizing: border-box;
+                color: #ffffff;
+                line-height: 42px;
+                -webkit-tap-highlight-color: rgba(255, 255, 255, 0);
+            }
+            button {
+                font-family: inherit;
+                font-size: 100%;
+                margin: 0;
+            }
+        </style>
     </head>
+    <body>
+        <img src="img/weixingongzhonghao/5.jpg" class="img-responsive center-block">
+        <button class="btn btn_primary" id="getNetworkType">getNetworkType</button>
      </body>
         <script type="text/javascript" src="https://res.wx.qq.com/open/js/jweixin-1.2.0.js"></script>  
         <script type="text/javascript">  
@@ -24,15 +62,27 @@ wx.config({
     signature: <?echo "'".$signature."'"?>,// 必填，签名
     jsApiList: ['openLocation','getLocation'] // 必填，需要使用的JS接口列表 这里填写需要用到的微信api openlocation为使用微信内置地图查看位置接口
 });
-$(function(){
-    setTimeout(function(){wx.openLocation({
-          latitude: 39.993850,
-          longitude: 116.470090,
-          name: '禾描工作室',
-          address: '北京市朝阳区里外里公寓一单元2001',
-          scale: 14,
-          infoUrl: 'http://www.homerus.cn'
-        });},2000);
-});
+// 7 地理位置接口
+  // 7.1 查看地理位置
+  document.querySelector('#openLocation').onclick = function () {
+    wx.openLocation({
+      latitude: 39.993850,
+      longitude: 116.470090,
+      name: '禾描家',
+      address: '北京市朝阳区里外里公寓一单元',
+      scale: 14,
+      infoUrl: 'http://www.homerus.cn'
+    });
+  };
+// $(function(){
+//     setTimeout(function(){wx.openLocation({
+//           latitude: 39.993850,
+//           longitude: 116.470090,
+//           name: '禾描工作室',
+//           address: '北京市朝阳区里外里公寓一单元2001',
+//           scale: 14,
+//           infoUrl: 'http://www.homerus.cn'
+//         });},2000);
+// });
     </script>
 </html>
