4bbe80a6354497f5f86ee891027e44fd25888503
janzen <349685719@qq.com>
2018-05-29 14:51:23 +0800

    wx

--- laravel/app/Http/Controllers/WxAboutController.php
+++ laravel/app/Http/Controllers/WxAboutController.php
@@ -9,10 +9,40 @@ class WxAboutController extends Controller
 {
     //
     public function wxMap(){
-    	
+    	$noncestr = $this->createNonceStr(16);
+    	$timestamp = time();
+    	$url = "http://homerus.cn/maptest";
     	$content = file_get_contents("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxcfa82ce1a3e84893&secret=b8cac9729fbcf6bac2c017b80d6b7752");
+		$contentArr = json_decode($content,true);
+		$access_token = $contentArr['access_token'];
+		$jsapi = file_get_contents("https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=$access_token&type=jsapi");
+		$jsapiArr = json_decode($jsapi,true);
+		$ticket = $jsapiArr['ticket'];
 
-dd(json_decode($content,true));
-    	return view('map');
+
+		$string='jsapi_ticket='.$ticket.'&noncestr='.$noncestr.'×tamp='.$timestamp.'&url='.$url;
+	    $signature = sha1($string);
+	    $signPackage = array(
+	        "appId"     =>"wxcfa82ce1a3e84893",
+	        "nonceStr"  =>$noncestr,
+	        "timestamp" => $timestamp,
+	        "url"       => $url,
+	        "signature" => $signature,
+	        "string" => $string
+	    );
+
+
+    	return view('map',$signPackage);
     }
+
+
+
+    private function createNonceStr($length = 16) {
+	    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	    $str = "";
+	    for ($i = 0; $i < $length; $i++) {
+	      $str .= substr($chars, mt_rand(0, strlen($chars) - 1), 1);
+	    }
+	    return $str;
+	  }
 }
--- laravel/resources/views/map.blade.php
+++ laravel/resources/views/map.blade.php
@@ -10,12 +10,30 @@
     </style> </head><body>  
    <div id="allmap"></div></body>  </html>  
    <script type="text/javascript">  
-wx.openLocation({
-    latitude: 0, // 纬度，浮点数，范围为90 ~ -90
-    longitude: 0, // 经度，浮点数，范围为180 ~ -180。
-    name: '', // 位置名
-    address: '', // 地址详情说明
-    scale: 1, // 地图缩放级别,整形值,范围从1~28。默认为最大
-    infoUrl: '' // 在查看位置界面底部显示的超链接,可点击跳转
+wx.config({
+    debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+    appId: <?echo $signPackage['appId']?>, // 必填，公众号的唯一标识
+    timestamp: <?echo $signPackage['timestamp']?>, // 必填，生成签名的时间戳
+    nonceStr: <?echo $signPackage['nonceStr']?>, // 必填，生成签名的随机串
+    signature: <?echo $signPackage['signature']?>,// 必填，签名
+    jsApiList: ['openLocation'] // 必填，需要使用的JS接口列表 这里填写需要用到的微信api openlocation为使用微信内置地图查看位置接口
+});
+
+wx.ready(function () {
+        wx.checkJsApi({
+            jsApiList: [
+                'openLocation',
+            ]// 需要检测的JS接口列表，所有JS接口列表见附录2,
+        });
+
+        wx.openLocation({
+            latitude: 0, // 纬度，浮点数，范围为90 ~ -90
+            longitude: 0, // 经度，浮点数，范围为180 ~ -180。
+            name: '', // 位置名
+            address: '', // 地址详情说明
+            scale: 1, // 地图缩放级别,整形值,范围从1~28。默认为最大
+            infoUrl: '' // 在查看位置界面底部显示的超链接,可点击跳转
+            });
     });
+
     </script>
