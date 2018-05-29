be7ce9ffa8a208bc98fa0fc7c43ffdfce3eac206
janzen <349685719@qq.com>
2018-05-29 21:08:15 +0800

    wx

--- laravel/resources/views/map.blade.php
+++ laravel/resources/views/map.blade.php
@@ -12,9 +12,7 @@
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
         <script src="js/jquery.js"></script>
 
-    </head><body ontouchstart=""><h3 id="menu-location">地理位置接口</h3>
-      <span class="desc">使用微信内置地图查看位置接口</span>
-      <button class="btn btn_primary" id="openLocation">openLocation</button>
+    </head>
      </body>
         <script type="text/javascript" src="https://res.wx.qq.com/open/js/jweixin-1.2.0.js"></script>  
         <script type="text/javascript">  
@@ -26,56 +24,15 @@ wx.config({
     signature: <?echo "'".$signature."'"?>,// 必填，签名
     jsApiList: ['openLocation','getLocation'] // 必填，需要使用的JS接口列表 这里填写需要用到的微信api openlocation为使用微信内置地图查看位置接口
 });
-// wx.openLocation({
-//       latitude: 23.099994,
-//       longitude: 113.324520,
-//       name: 'TIT 创意园',
-//       address: '广州市海珠区新港中路 397 号',
-//       scale: 14,
-//       infoUrl: 'http://weixin.qq.com'
-//     });
-// 7 地理位置接口
-  // 7.1 查看地理位置
-  // document.querySelector('#openLocation').onclick = function () {
-    // wx.openLocation({
-    //   latitude: 23.099994,
-    //   longitude: 113.324520,
-    //   name: 'TIT 创意园',
-    //   address: '广州市海珠区新港中路 397 号',
-    //   scale: 14,
-    //   infoUrl: 'http://weixin.qq.com'
-    // });
 $(function(){
-setTimeout(function(){wx.openLocation({
-      latitude: 39.993850,
-      longitude: 116.470090,
-      name: '禾描工作室',
-      address: '北京市朝阳区里外里公寓一单元2001',
-      scale: 14,
-      infoUrl: 'http://www.homerus.cn'
-    });},2000);
+    setTimeout(function(){wx.openLocation({
+          latitude: 39.993850,
+          longitude: 116.470090,
+          name: '禾描工作室',
+          address: '北京市朝阳区里外里公寓一单元2001',
+          scale: 14,
+          infoUrl: 'http://www.homerus.cn'
+        });},2000);
 });
-
-  // };
-// wx.getLocation({  
-//             type : 'gcj02', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'  
-//             success : function(res) {  
-//                 alert("00");  
-//                 //使用微信内置地图查看位置接口  
-//                 wx.openLocation({  
-//                     latitude : res.latitude, // 纬度，浮点数，范围为90 ~ -90  
-//                     longitude : res.longitude, // 经度，浮点数，范围为180 ~ -180。  
-//                     name : '我的位置', // 位置名  
-//                     address : '329创业者社区', // 地址详情说明  
-//                     scale : 28, // 地图缩放级别,整形值,范围从1~28。默认为最大  
-//                     infoUrl : 'http://www.gongjuji.net' // 在查看位置界面底部显示的超链接,可点击跳转（测试好像不可用）  
-//                 });  
-//                 alert("11");  
-//             },  
-//             cancel : function(res) {  
-//                 alert("22");  
-//             }  
-//         }); 
-
     </script>
 </html>
