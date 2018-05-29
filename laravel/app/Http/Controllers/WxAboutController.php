e04b7c8cada53b694b18336d0fa6cd1112d4ceab
janzen <349685719@qq.com>
2018-05-29 13:44:35 +0800

    wx

--- laravel/app/Http/Controllers/WxAboutController.php
+++ laravel/app/Http/Controllers/WxAboutController.php
@@ -12,7 +12,7 @@ class WxAboutController extends Controller
     	
     	$content = file_get_contents("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxcfa82ce1a3e84893&secret=b8cac9729fbcf6bac2c017b80d6b7752");
 
-dd($content);
+dd(json_decode($content,true));
     	return view('map');
     }
 }
