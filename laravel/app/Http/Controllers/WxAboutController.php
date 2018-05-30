f52a1457923126c071c65486db96f092e9043372
janzen <349685719@qq.com>
2018-05-30 18:55:04 +0800

    wx

--- laravel/app/Http/Controllers/WxAboutController.php
+++ laravel/app/Http/Controllers/WxAboutController.php
@@ -8,10 +8,10 @@ use App\Http\Controllers\Controller;
 class WxAboutController extends Controller
 {
     //
-    public function wxMap(){
+    public function wxMap(Request $request){
     	$noncestr = $this->createNonceStr(16);
     	$timestamp = time();
-    	dd(Request::getRequestUri());
+    	dd($request->getRequestUri());
     	$url = "https://www.homerus.cn/hemiaomap";
     	$content = file_get_contents("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxcfa82ce1a3e84893&secret=b8cac9729fbcf6bac2c017b80d6b7752");
 		$contentArr = json_decode($content,true);
