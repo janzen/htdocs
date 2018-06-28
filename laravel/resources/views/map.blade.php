575a6340e4084701db345b088eba2c9255c1546d
janzen <349685719@qq.com>
2018-06-28 15:50:21 +0800

    地址

--- laravel/resources/views/map.blade.php
+++ laravel/resources/views/map.blade.php
@@ -207,13 +207,17 @@
         img {
             border: 0;
         }
-
+        
+        .pfont{
+          font-size: 12px;
+          color: #3e3e3e;
+        }
 </style>
 </head>
 <body ontouchstart="">
     <img src="img/weixingongzhonghao/dizhi.jpg" class="img-responsive center-block">
     <div class="img-responsive center-block">
-      <h1>线下店在哪儿</h1>
+      <h3>线下店在哪儿</h3>
       <p>为了帮助客户节约成本，以及创建更真实的空间感，禾描不设立独立卖场，体验店接受预约参观。</p>
       <p>营业时间：13:30～22:30（其他时间也接受预约）</p>
       <p>预约电话：15110178004（微信同号）</p>
