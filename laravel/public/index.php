e89f9dc35e88ad3c08cb82ee15d5587d83e86f83
janzen <349685719@qq.com>
2019-12-02 14:04:09 +0800

    验证

--- laravel/public/index.php
+++ laravel/public/index.php
@@ -20,7 +20,7 @@
 */
 
 
-// require __DIR__.'/../bootstrap/autoload.php';
+require __DIR__.'/../bootstrap/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -34,7 +34,7 @@
 |
 */
 
-// $app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -48,12 +48,12 @@
 |
 */
 
-// $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-// $response = $kernel->handle(
-//     $request = Illuminate\Http\Request::capture()
-// );
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
 
-// $response->send();
+$response->send();
 
-// $kernel->terminate($request, $response);
+$kernel->terminate($request, $response);
